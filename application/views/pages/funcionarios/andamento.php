9d945128bfcc0ad92d98a86c5e088aa9ae36db25
gabriel-baltar <48927391+gabriel-baltar@users.noreply.github.com>
2019-05-10 16:52:07 -0300

    Atualização 10/05/2019

--- application/views/pages/funcionarios/andamento.php
+++ application/views/pages/funcionarios/andamento.php
@@ -3,6 +3,58 @@
         <h3 class="card-header text-white bg-success">
             Chamados em andamento
         </h3>
-        <h1>Chamados em andamento</h1>
+        <h5>
+            <div class="mt-2 ml-5 mr-5 mb-5">
+                <table class="table table-bondered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Descrição</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Problemas de Hardware</td>
+                            <td class="text-success">Em Andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Problemas com Software</td>
+                            <td class="text-warning">Em Fila</td>
+                        </tr>
+                        <tr>
+                            <td>Periféricos</td>
+                            <td class="text-danger">Em Análise</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </h5>
     </div>
 </div>
--- application/views/pages/funcionarios/ferias.php
+++ application/views/pages/funcionarios/ferias.php
@@ -1,53 +1,57 @@
 <div class="container mt-2 border-2">
-    <div class="card">
-        <h3 class="card-header text-white bg-success">
-            Ferias
-        </h3>
-        <div class="form-group row mt-3 ml-5 mb-3">
-  <label for="example-date-input" class="col-2 col-form-label"><h3 class="col-md-12">Início:</h3></label>
-  <div class="col-md-6 mt-2 mb-3">
-    <input class="form-control" type="date" value="2019-05-06" id="example-date-input">
-  </div>
-</div>
-<div class="form-group row  ml-5 mb-3">
-  <label for="example-date-input" class="col-2 col-form-label"><h3 class="col-md-12">Término:</h3></label>
-  <div class="col-md-6 mt-2 mb-3">
-    <input class="form-control" type="date" value="2019-05-06" id="example-date-input">
-  </div>
-  <button type="button" class="btn btn-success mt-2 mb-5">Efetuar Pedido</button>
-</div>
+	<div class="card">
+		<h3 class="card-header text-white bg-success">
+			Férias
+		</h3>
+		<div class="form-group row mt-3 ml-5 mb-3">
+			<label for="example-date-input" class="col-2 col-form-label">
+				<h3 class="col-md-12">Início:</h3>
+			</label>
+			<div class="col-md-6 mt-2 mb-3">
+				<input class="form-control" type="date" value="2019-05-06" id="example-date-input">
+			</div>
+		</div>
+		<div class="form-group row  ml-5 mb-3">
+			<label for="example-date-input" class="col-2 col-form-label">
+				<h3 class="col-md-12">Término:</h3>
+			</label>
+			<div class="col-md-6 mt-2 mb-3">
+				<input class="form-control" type="date" value="2019-05-06" id="example-date-input">
+			</div>
+			<button type="button" class="btn btn-success mt-2 mb-5">Efetuar Pedido</button>
+		</div>
 
-<div class=" mr-5 ml-5  mb-5">
-<table class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">Nome:</th>
-      <th scope="col">Data de Início:</th>
-      <th scope="col">Data do término:</th>
-      <th scope="col">Status:</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">Gabriel</th>
-      <td>12/10/2019</td>
-      <td>12/11/2019</td>
-      <td>Aprovado</td>
-    </tr>
-    <tr>
-      <th scope="row">Carlos</th>
-      <td>12/10/2019</td>
-      <td>12/11/2019</td>
-      <td>Aprovado</td>
-    </tr>
-    <tr>
-    <th scope="row">Carlos</th>
-      <td>12/10/2019</td>
-      <td>12/11/2019</td>
-      <td>Aprovado</td>
-    </tr>
-  </tbody>
-</table>
-    </div>
-</div>
+		<div class="mr-5 ml-5  mb-5">
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th scope="col">Nome:</th>
+						<th scope="col">Data de Início:</th>
+						<th scope="col">Data do término:</th>
+						<th scope="col">Status:</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">Gabriel</th>
+						<td>12/10/2019</td>
+						<td>12/11/2019</td>
+						<td>Aprovado</td>
+					</tr>
+					<tr>
+						<th scope="row">Carlos</th>
+						<td>12/10/2019</td>
+						<td>12/11/2019</td>
+						<td>Aprovado</td>
+					</tr>
+					<tr>
+						<th scope="row">Carlos</th>
+						<td>12/10/2019</td>
+						<td>12/11/2019</td>
+						<td>Aprovado</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
