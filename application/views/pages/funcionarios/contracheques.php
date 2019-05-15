8849a86d97d9ce674232f210af4c4d45c80ca8d8
gabriel-baltar <48927391+gabriel-baltar@users.noreply.github.com>
2019-05-15 17:03:19 -0300

    Atualização 15/05/2019

--- application/views/pages/funcionarios/contracheques.php
+++ application/views/pages/funcionarios/contracheques.php
@@ -1,15 +1,17 @@
 <div class="container mt-2 border-2">
-        <div class="card">
-          <h3 class="card-header text-white bg-success">
-            Contra Cheques
-          </h3>
-        <div>
-</div>
+	<div class="card">
+		<h3 class="card-header text-white bg-success">
+			Contra Cheques
+		</h3>
+		<div>
+		</div>
 
-<div class="form-group row mt-3 ml-5 mb-3">
-  <label for="example-date-input" class="col-2 col-form-label"><h3 class="col-md-8">Data:</h3></label>
-  <div class="col-md-7 mt-2 mb-3">
-    <input class="form-control" type="date" value="2019-05-06" id="example-date-input">
-  </div>
-  <button type="button" class="btn btn-success mt-2 mb-5">Documento</button>
-</div>
+		<div class="form-group row mt-3 ml-5 mb-3">
+			<label for="example-date-input" class="col-2 col-form-label">
+				<h3 class="col-md-8">Data:</h3>
+			</label>
+			<div class="col-md-7 mt-2 mb-3">
+				<input class="form-control" type="date" value="2019-05-06" id="example-date-input">
+			</div>
+			<button type="button" class="btn btn-success mt-2 mb-5">Documento</button>
+		</div>
